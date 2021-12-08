3214dec5cb1338e48b50bef7ca4ccccdfcd2faa2
DEYOF21 <daniee0801@osloskolen.no>
2021-12-08 10:31:23 +0100

    La til favicon bilde og la in kode for å linke til faviconet i index

--- PHP&CSS&JS-filer/index.php
+++ PHP&CSS&JS-filer/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="login-register-4.css"/>
+    <link rel="icon" type="image/x-icon" href="bilder-video/favicon.ico">
     <title>Welcome to PAVEN.CEO</title>
   </head>
 
