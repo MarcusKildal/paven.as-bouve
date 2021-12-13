25272b11e0cae3a066f45ccfef6f7f10cb7a8bf3
DEYOF21 <daniee0801@osloskolen.no>
2021-12-13 10:51:15 +0100

    La til litt bedre beskrivelser i de fleste andre divene

--- PHP&CSS&JS-filer/main.php
+++ PHP&CSS&JS-filer/main.php
@@ -208,7 +208,7 @@ if(empty($_SESSION["id"])) {
 
       <div class="api-left">
         <div class="api-text">Water power</div>
-        <p>This is the price of the water power right now.</p>
+        <p>Water power, is a renewable source of energy that generates power by using a dam or diversion structure to alter the natural flow of a river or other body of water. Waterpower relies on the endless, constantly recharging system of the water cycle to produce electricity, using a fuel—water—that is not reduced or eliminated in the process.</p>
       </div>
         
     </div>
@@ -233,7 +233,7 @@ if(empty($_SESSION["id"])) {
 
       <div class="api-left">
         <div class="api-text">Earnings</div>
-        <p>This is how much money we have earned the power.</p>
+        <p>This is how much money we have earned from the power we have sold.</p>
       </div>
         
     </div>
@@ -257,7 +257,7 @@ if(empty($_SESSION["id"])) {
 
       <div class="api-left">
         <div class="api-text">Nature cost</div>
-        <p>This is how much it has cost the nature.</p>
+        <p>This is how much we have to pay because of the effect on the environment.</p>
       </div>
         
     </div>
